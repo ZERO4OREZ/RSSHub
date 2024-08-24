ZERO4OREZ/RSSHub

commit 4c1a4963e36415a3d7bd4b3733ebb17e604bb7d0

feat: add width and height to img

lib/routes/telegram/channel.ts
@@ -428,7 +428,9 @@ async function handler(ctx) {
                                 const background = $node.css('background-image');
                                 const backgroundUrl = background && background.match(/url\('(.*)'\)/);
                                 const backgroundUrlSrc = backgroundUrl && backgroundUrl[1];
-                                tag_media += backgroundUrlSrc ? `<img src="${backgroundUrlSrc}">` : '';
+                                const width = Number.parseFloat($node.css('width') || '0');
+                                const height = ((Number.parseFloat($node.find('.tgme_widget_message_photo').css('padding-top') || '0') / 100) * width).toFixed(2);
+                                tag_media += backgroundUrlSrc ? `<img width="${width}" height="${height}" src="${backgroundUrlSrc}">` : '';
                             }
                             if (tag_media) {
                                 tag_media_all += tag_media;

lib/routes/bing/daily-wallpaper.ts
@@ -57,7 +57,7 @@ async function handler(ctx) {
     const items = resp.MediaContents.map((item) => {
         const ssd = item.Ssd;
         const link = `${apiUrl}${item.ImageContent.Image.Url.match(/\/th\?id=[^_]+_[^_]+/)[0].replace(/(_\d+x\d+\.webp)$/i, '')}_${type}.jpg`;
-        let description = `<img src="${link}" alt="Article Cover Image" style="display: block; margin: 0 auto;"><br>`;
+        let description = `<img width="1920" height="1080" src="${link}" alt="Article Cover Image" style="display: block; margin: 0 auto;"><br>`;
         if (story) {
             description += `<b>${item.ImageContent.Headline}</b>`;
             description += `<i>${item.ImageContent.QuickFact.MainText}</i><br>`;

lib/routes/pixiv/utils.ts
@@ -2,15 +2,15 @@ import { config } from '@/config';
 
 export default {
     getImgs(illust) {
-        const images = [];
+        const images: string[] = [];
         if (illust.meta_pages?.length) {
             for (const page of illust.meta_pages) {
                 const original = page.image_urls.original.replace('https://i.pximg.net', config.pixiv.imgProxy);
-                images.push(`<p><img src="${original}"/></p>`);
+                images.push(`<p><img src="${original}" width="${page.width}" height="${page.height}" /></p>`);
             }
         } else if (illust.meta_single_page.original_image_url) {
             const original = illust.meta_single_page.original_image_url.replace('https://i.pximg.net', config.pixiv.imgProxy);
-            images.push(`<p><img src="${original}"/></p>`);
+            images.push(`<p><img src="${original}" width="${illust.width}" height="${illust.height}" /></p>`);
         }
         return images;
     },

lib/routes/twitter/utils.ts
@@ -104,7 +104,7 @@ const ProcessFeed = (ctx, { data = [] }, params = {}) => {
             if (!readable) {
                 content += '<br>';
             }
-            content += `<video src='${video.url}' ${gifAutoPlayAttr} controls='controls' poster='${getOriginalImg(media.media_url_https)}' ${extraAttrs}></video>`;
+            content += `<video width="${media.sizes.large.w}" height="${media.sizes.large.h}" src='${video.url}' ${gifAutoPlayAttr} controls='controls' poster='${getOriginalImg(media.media_url_https)}' ${extraAttrs}></video>`;
         }
 
         return content;
@@ -141,6 +141,9 @@ const ProcessFeed = (ctx, { data = [] }, params = {}) => {
                         if (heightOfPics > 0) {
                             content += `height="${heightOfPics}" `;
                             style += `height: ${heightOfPics}px;`;
+                        }
+                        if (widthOfPics <= 0 && heightOfPics <= 0) {
+                            content += `width="${media.sizes.large.w}" height="${media.sizes.large.h}" `;
                         }
                         content += ` style="${style}" ` + `${readable ? 'hspace="4" vspace="8"' : ''} src="${originalImg}">`;
                         if (addLinkForPics) {

commit 40c11183ce68575e1b9bd8c51867490d623f1d8b

fix(route/telegram/channel): invisible images in albums and Link Preview (#16523)
Some have reported that a recent upgrade of RSSHub broke the
/telegram/channel route, resulting in images from Link Preview (Instant
View) or albums not being displayed, even when showLinkPreview
explicitly or implicitly (by default) enabled.

This is a REGRESSION introduced in
4c1a4963e36415a3d7bd4b3733ebb17e604bb7d0.

The patch fixes it.

Signed-off-by: Rongrong <i@rong.moe>

lib/routes/telegram/channel.ts
@@ -428,9 +428,40 @@ async function handler(ctx) {
                                 const background = $node.css('background-image');
                                 const backgroundUrl = background && background.match(/url\('(.*)'\)/);
                                 const backgroundUrlSrc = backgroundUrl && backgroundUrl[1];
-                                const width = Number.parseFloat($node.css('width') || '0');
-                                const height = ((Number.parseFloat($node.find('.tgme_widget_message_photo').css('padding-top') || '0') / 100) * width).toFixed(2);
-                                tag_media += backgroundUrlSrc ? `<img width="${width}" height="${height}" src="${backgroundUrlSrc}">` : '';
+                                const attrs = [`src="${backgroundUrlSrc}"`];
+                                /*
+                                 * If the width is not in px, it is either a percentage (Link Preview/Instant view)
+                                 * or absent (ditto).
+                                 * Only accept px to prevent images from being invisible or too small.
+                                 */
+                                let width = 0;
+                                const widthStr = $node.css('width');
+                                if (widthStr && widthStr.endsWith('px')) {
+                                    width = Number.parseFloat(widthStr);
+                                }
+                                /*
+                                 * Height is present when the message is an album but does not exist in other cases.
+                                 * Ditto, only accept px.
+                                 * !!!NOTE: images in albums may have smaller width and height.
+                                 */
+                                let height = 0;
+                                const heightStr = $node.css('height');
+                                if (heightStr && heightStr.endsWith('px')) {
+                                    height = Number.parseFloat(heightStr);
+                                }
+                                /*
+                                 * Only calculate height when needed.
+                                 * The aspect ratio is either a percentage (single image) or absent (Link Preview).
+                                 * Only accept percentage to prevent images from being invisible or distorted.
+                                 */
+                                const aspectRatioStr = $node.find('.tgme_widget_message_photo').css('padding-top');
+                                if (height <= 0 && width > 0 && aspectRatioStr && aspectRatioStr.endsWith('%')) {
+                                    height = (Number.parseFloat(aspectRatioStr) / 100) * width;
+                                }
+                                // Only set width/height when >32 to avoid invisible images.
+                                width > 32 && attrs.push(`width="${width}"`);
+                                height > 32 && attrs.push(`height="${height.toFixed(2).replace('.00', '')}"`);
+                                tag_media += backgroundUrlSrc ? `<img ${attrs.join(' ')}>` : '';
                             }
                             if (tag_media) {
                                 tag_media_all += tag_media;